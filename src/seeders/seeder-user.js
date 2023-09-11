6ea8b35e510f14a54fc240b9a22c0f0c3059c72e
meobientnk34 <tranhaimaotnk34@gmail.com>
2023-09-11 08:43:56 +0700

    sequelize migration

---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Hai",
+        lastName: "Mao",
+        address: "Ninh Gia",
+        phoneNumber: "0123456789",
+        gender: 1,
+        image: "image",
+        roleId: "R1",
+        positionId: "P0",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
